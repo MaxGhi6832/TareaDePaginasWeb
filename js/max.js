1cd4611bd1955592b4a4efff1f1144c4f96a5333
MaxGhi6832 <mf527884@gmail.com>
2024-11-15 18:08:11 -0400

    Add files via upload

---
+++ js/max.js
@@ -0,0 +1,12 @@
+function aaa(){
+	var yesca=document.getElementById('nn').value;
+	var nano=document.getElementById('mm').value;
+	 if(yesca=="max"){
+	 	if(nano=="max123"){
+	 		alert("correcto");
+	 		location.href="menu.html";
+	 	}
+	 	else
+	 		alert("error prueve otra vez");
+	 } 
+}
